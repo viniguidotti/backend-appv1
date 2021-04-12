a1725e7713a6160005a2467d93a59433116c2f16
Vinicius Guidotti Moraes <vinicius@zapway.com.br>
2021-04-12 15:34:14 -0300

    First Commit - V1 BackEnd

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./app/controllers')(app);
+
+const PORT = process.env.PORT;
+if (PORT) {
+    app.listen(PORT, () => {
+        console.log(`The app is running on port ${ PORT }`);
+    });
+} else {
+    app.listen(3000);
+    console.log("The app is running on port 3000")
+}
+
